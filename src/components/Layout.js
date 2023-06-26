84487d0ed381e5bf2573677c214399f75ca501ec
Hitesh Meta <meta.hitesh85@gmail.com>
2023-06-26 21:57:11 +0530

    Add Prompt Component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Prompt from './Prompt';
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Prompt />
       <Navbar />
       {children}
     </>
