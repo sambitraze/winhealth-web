4480834b168b699c5822cbc961facca8dcba2f3b
Hitesh Meta <meta.hitesh85@gmail.com>
2023-06-26 23:40:05 +0530

    Setup Mantine - And Install Mantine Carousel

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,17 @@
 import '../styles/globals.css';
 import { brandon, proxima, ptSans } from '../utils/fonts';
 import { TailwindIndicator } from '../components/TailwindIndicator';
+import { MantineProvider } from '@mantine/core';
 
 export default function App({ Component, pageProps }) {
   return (
-    <main
-      className={`${brandon.variable} ${proxima.variable} ${ptSans.variable} font-brandon`}
-    >
-      <Component {...pageProps} />
-      <TailwindIndicator />
-    </main>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <main
+        className={`${brandon.variable} ${proxima.variable} ${ptSans.variable} font-brandon`}
+      >
+        <Component {...pageProps} />
+        <TailwindIndicator />
+      </main>
+    </MantineProvider>
   );
 }
