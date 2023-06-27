7e69dec843e7bde41a09dc51dd5a6237f8e7bd49
Hitesh Meta <meta.hitesh85@gmail.com>
2023-06-27 21:58:50 +0530

    Move Meta Info to Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,10 +2,20 @@ import React from 'react';
 import Navbar from './Navbar';
 import Prompt from './Prompt';
 import Footer from './Footer';
+import Head from 'next/head';
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>WinHealth</title>
+        <meta
+          name="description"
+          content="WinHealth- Unleash Gut Health & Heal your Gut (HOg)"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Prompt />
       <Navbar />
       <div className="min-h-screen">{children}</div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,19 +4,8 @@ import TestimonialSection from '../components/Testimonials/TestimonialSection';
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>WinHealth</title>
-        <meta
-          name="description"
-          content="WinHealth- Unleash Gut Health & Heal your Gut (HOg)"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Layout>
-        <TestimonialSection />
-      </Layout>
-    </>
+    <Layout>
+      <TestimonialSection />
+    </Layout>
   );
 }
