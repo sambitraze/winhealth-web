adbbab54e5ffdb9bc8b5d8de429b64670a3ec866
Hitesh Meta <meta.hitesh85@gmail.com>
2023-06-27 11:07:03 +0530

    Add Founders Message Section

--- src/pages/about-us/index.js
+++ src/pages/about-us/index.js
@@ -2,11 +2,13 @@ import Layout from '../../components/Layout';
 import OurValuesSection from '../../components/AboutUs/OurValuesSection';
 import OurTeamSection from '../../components/AboutUs/OurTeamSection';
 import OurFoundersSection from '../../components/AboutUs/OurFoundersSection';
+import FoundersMessageSection from '../../components/AboutUs/FoundersMessageSection';
 
 const AboutUs = () => {
   return (
     <Layout>
       <OurFoundersSection />
+      <FoundersMessageSection />
       <OurTeamSection />
       <OurValuesSection />
     </Layout>
