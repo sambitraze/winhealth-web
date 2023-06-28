96f7c8856d1f670a078922108300e711a4c003d9
Hitesh Meta <meta.hitesh85@gmail.com>
2023-06-28 19:11:46 +0530

    Add Herosection in Homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head';
 import Layout from '../components/Layout';
 import TestimonialSection from '../components/Testimonials/TestimonialSection';
+import HomepageHerosection from '../components/Homepage/HomepageHerosection';
 
 export default function Home() {
   return (
     <Layout>
+      <HomepageHerosection />
       <TestimonialSection />
     </Layout>
   );
