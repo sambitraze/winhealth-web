19ec69c59ba1933c4261f9676d7e187e33c99a62
Hitesh Meta <meta.hitesh85@gmail.com>
2023-06-28 22:30:49 +0530

    Add FAQ Herosection

---
+++ src/pages/faq/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+import FAQHerosection from '../../components/FAQ/FAQHerosection';
+
+const FAQ = () => {
+  return (
+    <Layout>
+      <FAQHerosection />
+    </Layout>
+  );
+};
+
+export default FAQ;
