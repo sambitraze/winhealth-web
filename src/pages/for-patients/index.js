8db6bd13d3c7ba2471703c05fa2ad1f3cb61f6ae
Hitesh Meta <meta.hitesh85@gmail.com>
2023-06-29 22:43:12 +0530

    Add Facilities Section

--- src/pages/for-patients/index.js
+++ src/pages/for-patients/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ForPatientsHerosection from '../../components/ForPatients/ForPatientsHerosection';
 import Layout from '../../components/Layout';
+import FacilitiesSection from '../../components/ForPatients/FacilitiesSection';
 
 const ForPatients = () => {
   return (
     <Layout>
       <ForPatientsHerosection />
+      <FacilitiesSection />
     </Layout>
   );
 };
