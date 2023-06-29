f516a9f3fe177b4259fc46bb2f2b9d02834fa22a
Hitesh Meta <meta.hitesh85@gmail.com>
2023-06-29 22:15:25 +0530

    Add For Patients Herosection

---
+++ src/pages/for-patients/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ForPatientsHerosection from '../../components/ForPatients/ForPatientsHerosection';
+import Layout from '../../components/Layout';
+
+const ForPatients = () => {
+  return (
+    <Layout>
+      <ForPatientsHerosection />
+    </Layout>
+  );
+};
+
+export default ForPatients;
