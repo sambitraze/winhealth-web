6a9f8d107e04a22a4b62ae84c0429290d17dce87
Hitesh Meta <meta.hitesh85@gmail.com>
2023-06-29 23:01:19 +0530

    Add Redesigned Digestive Care Section

--- src/pages/for-patients/index.js
+++ src/pages/for-patients/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ForPatientsHerosection from '../../components/ForPatients/ForPatientsHerosection';
 import Layout from '../../components/Layout';
 import FacilitiesSection from '../../components/ForPatients/FacilitiesSection';
+import RedesignedDigestiveCareSection from '../../components/ForPatients/RedesignedDigestiveCareSection';
 
 const ForPatients = () => {
   return (
     <Layout>
       <ForPatientsHerosection />
       <FacilitiesSection />
+      <RedesignedDigestiveCareSection />
     </Layout>
   );
 };
