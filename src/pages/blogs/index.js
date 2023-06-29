28062dc4c358c346b11560609048c7792e959b51
Hitesh Meta <meta.hitesh85@gmail.com>
2023-06-29 20:53:24 +0530

    Add Our Doctors Herosection

--- src/pages/blogs/index.js
+++ src/pages/blogs/index.js
@@ -3,12 +3,14 @@ import Layout from '../../components/Layout';
 import BlogsHerosection from '../../components/Blogs/BlogsHerosection';
 import LatestBlogsSection from '../../components/Blogs/LatestBlogsSection';
 import InThePressSection from '../../components/Blogs/InThePressSection';
+import OurAchievementsSection from '../../components/Blogs/OurAchievementsSection';
 
 const Index = () => {
   return (
     <Layout>
       <BlogsHerosection />
       <LatestBlogsSection />
+      <OurAchievementsSection />
       <InThePressSection />
     </Layout>
   );
---
+++ src/pages/for-doctors/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+import ForDoctorsHerosection from '../../components/ForDoctors/ForDoctorsHerosection';
+
+const ForPatients = () => {
+  return (
+    <Layout>
+      <ForDoctorsHerosection />
+    </Layout>
+  );
+};
+
+export default ForPatients;
