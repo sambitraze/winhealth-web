3dd1f48860272b4e0acea4874c215be30e94ef15
Hitesh Meta <meta.hitesh85@gmail.com>
2023-06-30 20:31:03 +0530

    Update Logo - Add Image

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Logo = () => {
   return (
     <Link href={'/'} className="text-xl font-semibold text-white">
-      LOGO
+      <Image
+        src={'/assets/logo.svg'}
+        alt="Logo"
+        width={100}
+        height={50}
+        className="h-auto cursor-pointer object-contain"
+      />
     </Link>
   );
 };
