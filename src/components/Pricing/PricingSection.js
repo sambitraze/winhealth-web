7f275c8af4bb624e4b51b07ac177a906a69e6756
Hitesh Meta <meta.hitesh85@gmail.com>
2023-07-01 13:17:22 +0530

    Add Pricing Section

--- src/components/Pricing/PricingSection.js
+++ src/components/Pricing/PricingSection.js
@@ -6,8 +6,10 @@ const PricingSection = () => {
   return (
     <div className="container py-10">
       <Title type={'section'} text={'Our Plans'} />
-      <div className="mx-auto max-w-screen-lg py-4 md:py-10">
-        <div className="grid grid-cols-4">
+
+      {/* Desktop */}
+      <div className="mx-auto hidden max-w-screen-lg py-10 md:block">
+        <div className="grid grid-cols-4 border-b border-black">
           <div className="flex flex-col justify-center bg-themeDarkPink px-4 py-3 text-white">
             <div className="text-lg font-bold">Compare Plans</div>
             <div className="text-base">
@@ -48,7 +50,7 @@ const PricingSection = () => {
             </div>
           </div>
         </div>
-        <div className="grid grid-cols-4">
+        <div className="grid grid-cols-4 border-b border-black">
           <div className="flex flex-col justify-center bg-themeDarkPink px-4 py-3 text-white">
             <div>Duration</div>
           </div>
@@ -64,7 +66,7 @@ const PricingSection = () => {
             <div className="text-[#858BA0]">Add-ons on Demand</div>
           </div>
         </div>
-        <div className="grid grid-cols-4">
+        <div className="grid grid-cols-4 border-b border-black">
           <div className="flex flex-col justify-center bg-themeDarkPink px-4 py-3 text-white">
             <div>Users per plan</div>
           </div>
@@ -79,7 +81,7 @@ const PricingSection = () => {
             <div className="text-[#858BA0]">Add-ons on Demand</div>
           </div>
         </div>
-        <div className="grid grid-cols-4">
+        <div className="grid grid-cols-4 border-b border-black">
           <div className="flex flex-col justify-center bg-themeDarkPink px-4 py-3 text-white">
             <div>Lorem Ipsum</div>
           </div>
@@ -114,6 +116,199 @@ const PricingSection = () => {
                 height={16}
                 className="mx-auto"
               />
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-4 border-b border-black">
+          <div className="flex flex-col justify-center bg-themeDarkPink px-4 py-3 text-white">
+            <div>Lorem Ipsum</div>
+          </div>
+          <div className="flex flex-col justify-center bg-[#B6F4FF] px-4 py-3 text-center text-sm leading-4">
+            <div></div>
+          </div>
+          <div className="flex flex-col justify-center bg-[#8EE2EB] px-4 py-3 text-center text-sm leading-4">
+            <div></div>
+          </div>
+          <div className="flex flex-col justify-center bg-[#B6F4FF] px-4 py-3 text-center text-sm leading-4">
+            <div>
+              <Image
+                src={'/assets/verified1.svg'}
+                alt="Verified Icon"
+                width={16}
+                height={16}
+                className="mx-auto"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile */}
+      <div className="flex flex-col gap-8 py-4 md:hidden">
+        <div>
+          <div className="flex flex-col justify-center border-b border-black bg-[#B6F4FF] px-4 py-3 text-center text-sm leading-4">
+            <div>
+              <span className="font-brandon text-3xl font-bold">Free</span>
+              <span className="text-[#858BA0]">/ Lifetime</span>
+            </div>
+            <div className="mt-2">
+              <button className="w-full rounded-md bg-[#252430] py-3 font-medium text-white">
+                Choose This Plan
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center border-b border-black bg-[#B6F4FF] px-4 py-3 text-center text-sm leading-4">
+            <div>2 weeks</div>
+          </div>
+          <div className="flex flex-col items-center justify-center border-b border-black bg-[#B6F4FF] px-4 py-3 text-center text-sm leading-4">
+            <div>5 pages</div>
+          </div>
+          <div className="flex flex-col items-center justify-center border-b border-black bg-[#B6F4FF] px-4 py-3 text-center text-sm leading-4">
+            <div className="flex items-center gap-2">
+              <Image
+                src={'/assets/verified1.svg'}
+                alt="Verified Icon"
+                width={16}
+                height={16}
+              />
+              <p>Lorem ipsum dolor</p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center border-b border-black bg-[#B6F4FF] px-4 py-3 text-center text-sm leading-4">
+            <div className="flex items-center gap-2">
+              <Image
+                src={'/assets/verified1.svg'}
+                alt="Verified Icon"
+                width={16}
+                height={16}
+              />
+              <p>Lorem ipsum dolor</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="flex flex-col justify-center border-b border-black bg-[#B6F4FF] px-4 py-3 text-center text-sm leading-4">
+            <div>
+              <span className="font-brandon text-3xl font-bold">$25</span>
+              <span className="text-[#858BA0]">/ Month</span>
+            </div>
+            <div className="mt-2">
+              <button className="w-full rounded-md bg-[#252430] py-3 font-medium text-white">
+                Choose This Plan
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center border-b border-black bg-[#B6F4FF] px-4 py-3 text-center text-sm leading-4">
+            <div>2 weeks</div>
+          </div>
+          <div className="flex flex-col items-center justify-center border-b border-black bg-[#B6F4FF] px-4 py-3 text-center text-sm leading-4">
+            <div>5 pages</div>
+          </div>
+          <div className="flex flex-col items-center justify-center border-b border-black bg-[#B6F4FF] px-4 py-3 text-center text-sm leading-4">
+            <div className="flex items-center gap-2">
+              <Image
+                src={'/assets/verified1.svg'}
+                alt="Verified Icon"
+                width={16}
+                height={16}
+              />
+              <p>Lorem ipsum dolor</p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center border-b border-black bg-[#B6F4FF] px-4 py-3 text-center text-sm leading-4">
+            <div className="flex items-center gap-2">
+              <Image
+                src={'/assets/verified1.svg'}
+                alt="Verified Icon"
+                width={16}
+                height={16}
+              />
+              <p>Lorem ipsum dolor</p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center border-b border-black bg-[#B6F4FF] px-4 py-3 text-center text-sm leading-4">
+            <div className="flex items-center gap-2">
+              <Image
+                src={'/assets/verified1.svg'}
+                alt="Verified Icon"
+                width={16}
+                height={16}
+              />
+              <p>Lorem ipsum dolor</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="flex flex-col justify-center border-b border-black bg-[#B6F4FF] px-4 py-3 text-center text-sm leading-4">
+            <div>
+              <span className="font-brandon text-3xl font-bold">$40</span>
+              <span className="text-[#858BA0]">/ Month</span>
+            </div>
+            <div className="mt-2">
+              <button className="w-full rounded-md bg-[#252430] py-3 font-medium text-white">
+                Choose This Plan
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center border-b border-black bg-[#B6F4FF] px-4 py-3 text-center text-sm leading-4">
+            <div>2 weeks</div>
+          </div>
+          <div className="flex flex-col items-center justify-center border-b border-black bg-[#B6F4FF] px-4 py-3 text-center text-sm leading-4">
+            <div>5 pages</div>
+          </div>
+          <div className="flex flex-col items-center justify-center border-b border-black bg-[#B6F4FF] px-4 py-3 text-center text-sm leading-4">
+            <div className="flex items-center gap-2">
+              <Image
+                src={'/assets/verified1.svg'}
+                alt="Verified Icon"
+                width={16}
+                height={16}
+              />
+              <p>Lorem ipsum dolor</p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center border-b border-black bg-[#B6F4FF] px-4 py-3 text-center text-sm leading-4">
+            <div className="flex items-center gap-2">
+              <Image
+                src={'/assets/verified1.svg'}
+                alt="Verified Icon"
+                width={16}
+                height={16}
+              />
+              <p>Lorem ipsum dolor</p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center border-b border-black bg-[#B6F4FF] px-4 py-3 text-center text-sm leading-4">
+            <div className="flex items-center gap-2">
+              <Image
+                src={'/assets/verified1.svg'}
+                alt="Verified Icon"
+                width={16}
+                height={16}
+              />
+              <p>Lorem ipsum dolor</p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center border-b border-black bg-[#B6F4FF] px-4 py-3 text-center text-sm leading-4">
+            <div className="flex items-center gap-2">
+              <Image
+                src={'/assets/verified1.svg'}
+                alt="Verified Icon"
+                width={16}
+                height={16}
+              />
+              <p>Lorem ipsum dolor</p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center border-b border-black bg-[#B6F4FF] px-4 py-3 text-center text-sm leading-4">
+            <div className="flex items-center gap-2">
+              <Image
+                src={'/assets/verified1.svg'}
+                alt="Verified Icon"
+                width={16}
+                height={16}
+              />
+              <p>Lorem ipsum dolor</p>
             </div>
           </div>
         </div>
