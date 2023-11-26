79a1c0a44af50e80d22a76144c712e1cb28aeb46
SAMBIT MAJHI <majhisambit2@gmail.com>
2023-11-26 13:53:40 +0530

    up

---
+++ src/pages/cancellation-policy/index.js
@@ -0,0 +1,22 @@
+import Layout from '../../components/Layout';
+
+const CONTACTUS = () => {
+  return (
+    <Layout>
+      <div className="container py-8">
+        <h1>Contact Us</h1>
+        <p>Last updated on 26-11-2023 13:49:16</p>
+        <p>
+          You may contact us using the information below:
+
+          Merchant Legal entity name: MICROHEAL WELLNESS PRIVATE LIMITED
+          Registered Address: BUNGLAW NO. 18, 21 LIVING, SILVER SPRING BUNGLOWS, DIVINE ROAD, SCIENCE CITY, AHMEDABAD, AHMEDABAD, GUJARAT, 380060, AHMEDABAD, Gujarat, PIN: 380060
+          Operational Address: BUNGLAW NO. 18, 21 LIVING, SILVER SPRING BUNGLOWS, DIVINE ROAD, SCIENCE CITY, AHMEDABAD, AHMEDABAD, GUJARAT, 380060, AHMEDABAD, Gujarat, PIN: 380060
+          Telephone No: 7376113646
+          E-Mail ID: shashank08winger@gmail.com</p>
+      </div>
+    </Layout>
+  );
+};
+
+export default CONTACTUS;
---
+++ src/pages/contact-us/index.js
@@ -0,0 +1,32 @@
+import Layout from '../../components/Layout';
+
+const CanellationPolicy = () => {
+  return (
+    <Layout>
+      <div className="container py-8">
+        <h1>Cancellation & Refund Policy</h1>
+        <p>Last updated on 26-11-2023 13:51:45</p>
+        <p>
+          MICROHEAL WELLNESS PRIVATE LIMITED believes in helping its customers as far as possible,
+          and has therefore a liberal cancellation policy. Under this policy:
+          • Cancellations will be considered only if the request is made immediately after placing the order.
+          However, the cancellation request may not be entertained if the orders have been communicated to the
+          vendors/merchants and they have initiated the process of shipping them.
+          • MICROHEAL WELLNESS PRIVATE LIMITED does not accept cancellation requests for perishable
+          items like flowers, eatables etc. However, refund/replacement can be made if the customer establishes
+          that the quality of product delivered is not good.
+          • In case of receipt of damaged or defective items please report the same to our Customer Service team.
+          The request will, however, be entertained once the merchant has checked and determined the same at his
+          own end. This should be reported within 7 Days days of receipt of the products. In case you feel that the
+          product received is not as shown on the site or as per your expectations, you must bring it to the notice of
+          our customer service within 7 Days days of receiving the product. The Customer Service Team after
+          looking into your complaint will take an appropriate decision.
+          • In case of complaints regarding products that come with a warranty from manufacturers, please refer
+          the issue to them. In case of any Refunds approved by the MICROHEAL WELLNESS PRIVATE
+          LIMITED, it’ll take 6-8 Days days for the refund to be processed to the end customer.</p>
+      </div>
+    </Layout>
+  );
+};
+
+export default CanellationPolicy;
