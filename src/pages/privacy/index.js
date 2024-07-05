c6aebcefc2d3a5459fbcc0ab97e0f1ae36e8c057
SAMBIT MAJHI <majhisambit2@gmail.com>
2024-07-05 14:49:54 +0530

    Update index.js

--- src/pages/privacy/index.js
+++ src/pages/privacy/index.js
@@ -5,7 +5,7 @@ const FAQ = () => {
     <Layout>
       <div className="container py-8">
         <h1>Privacy Policy</h1>
-        <p>Last updated: October 04, 2023</p>
+        <p>Last updated: July 5, 2024</p>
 
         <p>This Privacy Policy describes Our policies and procedures on the collection, use, and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
         <p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy.</p>
